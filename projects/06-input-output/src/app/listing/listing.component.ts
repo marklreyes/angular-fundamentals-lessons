6540a757c72f573d1b466b2bfd0fa122f8aeec1d
Mark Reyes <mr@marklreyes.com>
2025-01-30 10:26:54 -0800

    Move markup to Listing Component and bring it back as a dependency to App Component

--- projects/06-input-output/src/app/listing/listing.component.ts
+++ projects/06-input-output/src/app/listing/listing.component.ts
@@ -6,7 +6,33 @@ import { Car } from '../car';
   selector: 'app-listing',
   standalone: true,
   imports: [CommonModule],
-  template: ` <!-- listing markup goes here --> `,
+  template: `
+      <article class="listing">
+        <div class="image-parent">
+          <img class="product-image" src="https://placehold.co/100x100" />
+        </div>
+        <section class="details">
+          <p class="title"><!-- car make and model--></p>
+          <hr />
+          <p class="detail">
+            <span>Year</span>
+            <span><!-- year --></span>
+          </p>
+          <div class="detail">
+            <span>Transmission</span>
+            <span><!-- transmission --></span>
+          </div>
+          <p class="detail">
+            <span>Mileage</span>
+            <span><!-- miles --></span>
+          </p>
+          <p class="detail">
+            <span>Price</span>
+            <span><!-- price --></span>
+          </p>
+        </section>
+      </article>
+  `,
   styles: ``,
 })
 export class ListingComponent {}
