3a888ca95d382d379cbc17b5c632981e46b899f9
Mark Reyes <mr@marklreyes.com>
2025-01-30 11:12:12 -0800

    Implement addCarToSaved() to the savedCarList (of type Car[]) property of AppComponent.

--- projects/06-input-output/src/app/app.component.ts
+++ projects/06-input-output/src/app/app.component.ts
@@ -10,7 +10,7 @@ import { ListingComponent } from "./listing/listing.component";
     <section class="container">
       <!-- This article element represents and entire listing -->
        @for (carEntry of carList; track carEntry) {
-        <app-listing [car]="carEntry"/>
+        <app-listing [car]="carEntry" (carSaved)="addCarToSaved($event)"/>
        }
       <!-- end car listing markup -->
     </section>
@@ -54,4 +54,8 @@ export class AppComponent {
       transmission: 'Automatic',
     },
   ];
+
+  addCarToSaved(car: Car) {
+    this.savedCarList.push(car);
+  }
 }
